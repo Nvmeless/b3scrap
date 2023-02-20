c6c50c555054b4548689e1803e8f6d1e8fd75aee
Nvmeless <59935640+Nvmeless@users.noreply.github.com>
2023-02-20 13:02:15 +0100

    pseudo Code

---
+++ scrap.py
@@ -0,0 +1,29 @@
+# Initialisation des variables
+
+# Definir la largeur(x) et la longueur de mon tableau
+# a deux dimensions
+
+# Creer une fonction qui me génère et renvoie un 
+# tableau a deux dimension, rempli de cellules mortes
+# ou vivantes 
+
+# creer une fonction qui itere sur chaque cellule de
+# mon tableau (ligne par ligne puis colonne par 
+#  colonne)
+
+# creer une fonction qui verifie les voisin d'une
+# cellule
+
+#definir une fonction prenant en parametre un tableau
+# qui permet
+#  d'Utiliser la fonction de verification dans la 
+# fonction d'iteration du tableu
+# et Renvoie une copie du tableau 
+
+
+# Execution
+# generer un tableau, l'assigner a une variable
+# une boucle infinie (while True: ) 
+# qui affiche le tablea de la fonction precedente 
+# avec comme parametre le tableau, l'assigner a une variable "tableauCopy"
+# definir que tableau est egal a tableau copy
